f9b4399c7e7444ccccd8f81a8293e266486e456c
Bolun Zhang <rzhangbolun@gmail.com>
2019-06-12 22:55:56 -0400

    Add hello-world endpoint. Add java format script.

--- src/main/java/io/github/balloonio/DropworldApplication.java
+++ src/main/java/io/github/balloonio/DropworldApplication.java
@@ -3,27 +3,29 @@ package io.github.balloonio;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import io.github.balloonio.resources.HelloWorldResource;
 
 public class DropworldApplication extends Application<DropworldConfiguration> {
 
-    public static void main(final String[] args) throws Exception {
-        new DropworldApplication().run(args);
-    }
+  public static void main(final String[] args) throws Exception {
+    new DropworldApplication().run(args);
+  }
 
-    @Override
-    public String getName() {
-        return "Dropworld";
-    }
+  @Override
+  public String getName() {
+    return "Dropworld";
+  }
 
-    @Override
-    public void initialize(final Bootstrap<DropworldConfiguration> bootstrap) {
-        // TODO: application initialization
-    }
+  @Override
+  public void initialize(final Bootstrap<DropworldConfiguration> bootstrap) {
+    // TODO: application initialization
+  }
 
-    @Override
-    public void run(final DropworldConfiguration configuration,
-                    final Environment environment) {
-        // TODO: implement application
-    }
-
+  @Override
+  public void run(final DropworldConfiguration configuration, final Environment environment) {
+    // TODO: implement application
+    final HelloWorldResource helloWorldResource =
+        new HelloWorldResource(configuration.getTemplate(), configuration.getDefaultName());
+    environment.jersey().register(helloWorldResource);
+  }
 }
--- src/main/java/io/github/balloonio/DropworldConfiguration.java
+++ src/main/java/io/github/balloonio/DropworldConfiguration.java
@@ -1,10 +1,28 @@
 package io.github.balloonio;
 
 import io.dropwizard.Configuration;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.*;
 import javax.validation.constraints.*;
 
 public class DropworldConfiguration extends Configuration {
-    // TODO: implement service configuration
+  // TODO: implement service configuration
+  @NotEmpty private String template;
+
+  public String getTemplate() {
+    return template;
+  }
+
+  public void setTemplate(String template) {
+    this.template = template;
+  }
+
+  @NotEmpty private String defaultName;
+
+  public String getDefaultName() {
+    return defaultName;
+  }
+
+  public void setDefaultName(String defaultName) {
+    this.defaultName = defaultName;
+  }
 }
